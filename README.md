500d0f6b0976b7dbb659478f7a3c67534b7418f4
bloodfeather <bloodfeather@protonmail.com>
2018-04-13 13:17:54 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# brainfuck-interpreter
+
+Baseed off of/ported from (with some improvements) [Thomas Cort's BFI](http://esoteric.sange.fi/brainfuck/impl/interp/BFI.c).
+
+This is a simple brainfuck interpreter that runs BF scripts and boasts a somewhat limited interactive interpreter, written in python.
+It clings very loosely to the "standard" brainfuck rules, with some exceptions, as outlines below.
+*Note: This was just a fun little evening project, so there is a LOT of room for improvement - which I hope to do sometime.*
+
+[This](https://esolangs.org/wiki/Brainfuck) site was pretty helpful.
+
+**Loads of fun stuff [here](http://esoteric.sange.fi/brainfuck/)**
+
+## Usage
+> ./bfi.py \[--repl\] \[--file \<file\>\]
+In repl mode, 'exit' may be typed in the console to exit (duh).
+
+## Limitations
+* Tape size is set to 300 by default - change the tapesize variable if you want something different
+* After assigning cells with chars using the ',' command, you will not be able to increment or decrement them. The exception to this is if the char is a number.
+
+## REPL Prompts
+In repl mode, the BF prompt looks like
+> (0)\> 
+with the current tape pointer position in  parentheses.
+And the input prompt (invoked by ',') being
+> input\> 
+
+##TODO
+* Store chars as their decimal equivalents (i.e. unicode decimal values), as opposed to Python's string objects. This will allow programs to print chars without input.
---
+++ bfi.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+## brainfuck-interpreter by bloodf3ther ##
+
+import sys
+
+src = False
+buff = [] #the buffer into which will be loaded the sanitized tokens
+tapelen = 300 #length of the tape
+tape = [0 for x in range(0, tapelen)] #generates the tape
+pointer = 0 #tape pointer
+execptr = 0 #instruction pointer, used with buff
+
+for arg in sys.argv:
+    if len(sys.argv) < 2:
+        print('Usage:\n--repl          Launches bfi in interactive mode.\n--file <file>   Executes BF code from a file.')
+        sys.exit()
+    elif sys.argv[1] == '--repl': #leaves src as False - for interactive repl mode
+        pass
+    elif sys.argv[1] == '--file':
+        try:
+            src = open(sys.argv[2], 'r')
+        except:
+            print('[!] Error opening file: {0}'.format(sys.argv[2]))
+            sys.exit()
+
+def buffstuff(stuff): #sanitizes code and populates the buffer
+    global buff
+    for token in stuff:
+        if token in ['<', '>', ',', '.', '+', '-', '[', ']']:
+            buff.append(token)
+        else:
+            pass #every non command token is ignored
+
+def evaluate(): #evaluates contents of the code buffer and does things accordingly
+    global buff, tape, pointer, execptr
+    brackstack = 0 #stack for matching brackets
+    while execptr < len(buff):
+        if buff[execptr] == '<': #'<' moves the pointer one cell to the left
+            pointer -= 1
+        elif buff[execptr] == '>': #'>' moves the pointer one cell to the right
+            pointer += 1
+        elif buff[execptr] == '+': #'+' increments the cell under pointer by one
+            tape[pointer] += 1
+        elif buff[execptr] == '-': #'-' decrements the cell under pointer by one
+            tape[pointer] -= 1
+        elif buff[execptr] == '.': #'.' prints the value of the cell under pointer
+            print(tape[pointer])
+        elif buff[execptr] == ',': #',' will store a value in the cell under pointer from user input
+            x = input('input> ')
+            try:
+                x = int(x) #will try to convert the value to int, otherwise it will be stored as char
+            except:
+                pass
+            tape[pointer] = x
+        elif buff[execptr] == '[': #'[' will jump past the matching ']' if value under pointer is zero
+            if tape[pointer] == 0:
+                execptr +=1
+                while buff[execptr] != ']' or brackstack > 0:
+                    if buff[execptr] == '[':
+                        brackstack += 1
+                    if buff[execptr] == ']':
+                        brackstack -= 1
+                    execptr += 1
+        elif buff[execptr] == ']': #']' will jump to the previous matching ']'
+            execptr -= 1
+            while brackstack > 0 or buff[execptr] != '[':
+                if buff[execptr] == '[':
+                    execptr -= 1
+                if buff[execptr] == ']':
+                    execptr += 1
+                execptr -= 1
+            execptr -=1
+        else:
+            print('[!] Error: Invalid Token {0}'.format(buff[execptr])) #will raise an error and exit if a noncommand token somehow got into the buffer
+        execptr += 1
+
+if src != False: #if a file was specified and found this will parse, load to buffer, and execute it before exiting
+    for line in src:
+        buffstuff(line)
+    evaluate()
+
+elif src == False: #repl mode
+    while True:
+        x = input('({0})> '.format(pointer))
+        if x == 'exit': #the wonderful exit command
+            sys.exit()
+        buffstuff(x)
+        evaluate()
